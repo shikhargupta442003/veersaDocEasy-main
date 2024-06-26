eb8185a671c260f7619ba9e7fa63b20da01672ae
shikhar Gupta <shikhargupta442003@gmail.com>
2024-06-26 23:39:05 +0530

    signup page complete

--- docEasy/app/src/main/java/com/example/doceasy/signUpDoctor/doctorSignUp.kt
+++ docEasy/app/src/main/java/com/example/doceasy/signUpDoctor/doctorSignUp.kt
@@ -1,17 +1,26 @@
 package com.example.doceasy.signUpDoctor
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -19,43 +28,89 @@ import androidx.compose.ui.unit.sp
 import com.example.doceasy.R
 
 @Composable
-fun docSignUp(){
-    Column(modifier= Modifier
-        .padding(horizontal = 20.dp)
-        .fillMaxWidth()){
-        Box (modifier=Modifier.padding(horizontal = 20.dp, vertical = 24.dp)){
-            Text("Create New Account", color = colorResource(id = R.color.theme_color1), fontSize = 24.sp, fontWeight = FontWeight.ExtraBold )
+fun docSignUp() {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Box(modifier = Modifier.padding(horizontal = 20.dp, vertical = 24.dp)) {
+            Text(
+                "Create New Account",
+                color = colorResource(id = R.color.theme_color1),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
         }
-        Text(text = "Full Name", fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier=Modifier.padding(vertical = 8.dp))
-        Box(modifier=Modifier.clip(RoundedCornerShape(12.dp)).background(color = colorResource(id = R.color.box_inside_color))){
+        Text(
+            text = "Full Name",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = colorResource(id = R.color.box_inside_color))
+        ) {
             OutlinedTextField(
                 value = "",
                 shape = RoundedCornerShape(12.dp),
                 modifier = Modifier
                     .background(color = colorResource(id = R.color.box_inside_color))
                     .fillMaxWidth(),
-                placeholder = { Text("Enter Your Full Name", color= colorResource(id = R.color.box_hint_color), fontWeight = FontWeight.Bold) },
+                placeholder = {
+                    Text(
+                        "Enter Your Full Name",
+                        color = colorResource(id = R.color.box_hint_color),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
                 onValueChange = {
                 }
             )
         }
         Spacer(modifier = Modifier.padding(vertical = 4.dp))
-        Text(text = "Gender", fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier=Modifier.padding(bottom = 8.dp))
-        Box(modifier=Modifier.clip(RoundedCornerShape(12.dp)).background(color = colorResource(id = R.color.box_inside_color))){
+        Text(
+            text = "Gender",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = colorResource(id = R.color.box_inside_color))
+        ) {
             OutlinedTextField(
                 value = "",
                 shape = RoundedCornerShape(12.dp),
                 modifier = Modifier
                     .background(color = colorResource(id = R.color.box_inside_color))
                     .fillMaxWidth(),
-                placeholder = { Text("Enter Your Gender", color= colorResource(id = R.color.box_hint_color), fontWeight = FontWeight.Bold) },
+                placeholder = {
+                    Text(
+                        "Enter Your Gender",
+                        color = colorResource(id = R.color.box_hint_color),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
                 onValueChange = {
                 }
             )
         }
         Spacer(modifier = Modifier.padding(vertical = 4.dp))
-        Text(text = "Date of Birth", fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier=Modifier.padding(bottom = 8.dp))
-        Box(modifier=Modifier.clip(RoundedCornerShape(12.dp)).background(color = colorResource(id = R.color.box_inside_color))) {
+        Text(
+            text = "Date of Birth",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = colorResource(id = R.color.box_inside_color))
+        ) {
             OutlinedTextField(
                 value = "",
                 shape = RoundedCornerShape(12.dp),
@@ -69,31 +124,66 @@ fun docSignUp(){
         }
 
         Spacer(modifier = Modifier.padding(vertical = 4.dp))
-        Text(text = "Mobile No.", fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier=Modifier.padding(bottom = 8.dp))
-        Box(modifier=Modifier.clip(RoundedCornerShape(12.dp)).background(color = colorResource(id = R.color.box_inside_color))){
+        Text(
+            text = "Mobile No.",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = colorResource(id = R.color.box_inside_color))
+        ) {
             OutlinedTextField(
                 value = "",
                 shape = RoundedCornerShape(12.dp),
                 modifier = Modifier
                     .background(color = colorResource(id = R.color.box_inside_color))
                     .fillMaxWidth(),
-                placeholder = { Text("Enter Your Phone Number", color= colorResource(id = R.color.box_hint_color), fontWeight = FontWeight.Bold) },
+                placeholder = {
+                    Text(
+                        "Enter Your Phone Number",
+                        color = colorResource(id = R.color.box_hint_color),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
                 onValueChange = {
                 }
             )
         }
-
+        val stroke = Stroke(
+            width = 5f,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(15f, 15f), 0f)
+        )
         Spacer(modifier = Modifier.padding(vertical = 4.dp))
-        Text(text = "Upload your profile picture", fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier=Modifier.padding(bottom = 8.dp))
-        OutlinedTextField(
-            value = "",
+        Text(
+            text = "Upload your profile picture",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Box(modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .padding(vertical = 8.dp)
+            .fillMaxWidth()
+            .height(110.dp)
+            .drawBehind {
+                drawRoundRect(color = Color(0xffADD8E6), style = stroke)
+            }
+            .clickable { }) {
+            
+        }
+        Button(
+            onClick = {},
             shape = RoundedCornerShape(12.dp),
             modifier = Modifier
-                .background(color = colorResource(id = R.color.box_inside_color))
-                .fillMaxWidth(),
-            placeholder = { Text("Enter Your Full Name", color= colorResource(id = R.color.box_hint_color), fontWeight = FontWeight.Bold) },
-            onValueChange = {
-            }
-        )
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+                .size(52.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.theme_color1))
+        ) {
+            Text(text = "Next", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+        }
     }
 }
