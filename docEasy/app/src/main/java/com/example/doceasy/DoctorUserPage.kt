74625e872ee5db038ecbbfed096c1c09ba91bc26
shikhar Gupta <shikhargupta442003@gmail.com>
2024-06-27 23:42:16 +0530

    final2

--- docEasy/app/src/main/java/com/example/doceasy/DoctorUserPage.kt
+++ docEasy/app/src/main/java/com/example/doceasy/DoctorUserPage.kt
@@ -1,57 +1,52 @@
 package com.example.doceasy
 
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.doceasy.R
-import com.example.doceasy.data.doctorData
 
 
 @Composable
-fun DoctorSignIn(navController: NavController) {
+fun DoctorUserPage(navController: NavController) {
 
 
     val context= LocalContext.current
 
-
-        Button(
-            onClick = {},
-            shape = RoundedCornerShape(12.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp)
-                .size(52.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.theme_color1))
-        ) {
-            Text(text = "User", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
-        }
+Column(modifier=Modifier.fillMaxSize(),verticalArrangement = Arrangement.Center) {
+    Button(
+        onClick = {
+            navController.navigate("userSignUp")
+        },
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+            .size(52.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.theme_color1))
+    ) {
+        Text(text = "User", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+    }
 
 
     Button(
-        onClick = {},
+        onClick = {
+            navController.navigate("docSignUpLoginTerms")
+        },
         shape = RoundedCornerShape(12.dp),
         modifier = Modifier
             .fillMaxWidth()
@@ -61,5 +56,7 @@ fun DoctorSignIn(navController: NavController) {
     ) {
         Text(text = "Doctor", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
     }
+
+}
 
 }
