0295b1e68412ad30088c5a7263a20c3e8807dda1
shikhar Gupta <shikhargupta442003@gmail.com>
2024-06-27 23:11:44 +0530

    final1

--- docEasy/app/src/main/java/com/example/doceasy/Login/UserSignIn.kt
+++ docEasy/app/src/main/java/com/example/doceasy/Login/UserSignIn.kt
@@ -1,14 +1,11 @@
 package com.example.doceasy.Login
 
 
-import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,13 +18,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -36,7 +28,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.doceasy.R
 import com.example.doceasy.data.doctorData
-import com.example.doceasy.data.saveDoctorData
 
 
 @Composable
@@ -129,9 +120,7 @@ fun userSignIn(navController: NavController) {
                     email = email,
                     password = password,
                 )
-                saveDoctorData(doctor, onSucess = { Toast.makeText(context, "Data saved successfully", Toast.LENGTH_SHORT).show()}, onFailure = {exception->
-                    Toast.makeText(context, "Error: ${exception.message}", Toast.LENGTH_SHORT).show()})
-                navController.navigate("docSignUpContactInfo")
+               navController.navigate("docSignUpContactInfo")
             },
             shape = RoundedCornerShape(12.dp),
             modifier = Modifier
